2888e6eff9433a220e10cab1b4f2b43f65945f50
Arthur Dezan <44935219882@escola928.edu>
2025-11-13 10:13:31 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aqualife',
+  webDir: 'www'
+};
+
+export default config;
